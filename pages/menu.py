04baf61d790b54af4736b2c0d7a27fa086daf975
AK_Maddy <99036358+abhileo00@users.noreply.github.com>
2025-07-19 22:27:24 +0530

    Update menu.py

--- pages/menu.py
+++ pages/menu.py
@@ -13,7 +13,7 @@ def render():
     tab1, tab2 = st.tabs(["View Menu", "Manage Items"])
     
     with tab1:
-        st.dataframe(menu_df, hide_index=True, use_container_width=True)
+        st.dataframe(menu_df)
     
     with tab2:
         if st.session_state.user_role not in ["admin", "staff"]:
@@ -23,7 +23,7 @@ def render():
         action = st.radio("Action", ["Add Item", "Edit Item"], horizontal=True)
         
         if action == "Add Item":
-            with st.form("add_item", border=True):
+            with st.form("add_item"):
                 name = st.text_input("Item Name")
                 description = st.text_area("Description")
                 category = st.text_input("Category")
@@ -31,7 +31,7 @@ def render():
                 prep_time = st.number_input("Prep Time (mins)", min_value=1, value=15)
                 available = st.checkbox("Available", True)
                 
-                if st.form_submit_button("Add Item", use_container_width=True):
+                if st.form_submit_button("Add Item"):
                     new_item = {
                         "item_id": f"ITEM_{datetime.now().strftime('%Y%m%d%H%M%S')}",
                         "name": name,
@@ -45,13 +45,12 @@ def render():
                     menu_df = pd.concat([menu_df, pd.DataFrame([new_item])], ignore_index=True)
                     menu_df.to_csv("data/menu.csv", index=False)
                     st.success("Item added to menu!")
-                    st.rerun()
         
         else:  # Edit Item
             item_id = st.selectbox("Select Item", menu_df['item_id'])
             item = menu_df[menu_df['item_id'] == item_id].iloc[0]
             
-            with st.form("edit_item", border=True):
+            with st.form("edit_item"):
                 name = st.text_input("Name", value=item['name'])
                 description = st.text_area("Description", value=item['description'])
                 category = st.text_input("Category", value=item['category'])
@@ -59,7 +58,7 @@ def render():
                 prep_time = st.number_input("Prep Time", value=int(item['prep_time']))
                 available = st.checkbox("Available", value=bool(item['available']))
                 
-                if st.form_submit_button("Update Item", use_container_width=True):
+                if st.form_submit_button("Update Item"):
                     menu_df.loc[menu_df['item_id'] == item_id, 'name'] = name
                     menu_df.loc[menu_df['item_id'] == item_id, 'description'] = description
                     menu_df.loc[menu_df['item_id'] == item_id, 'category'] = category
@@ -68,4 +67,3 @@ def render():
                     menu_df.loc[menu_df['item_id'] == item_id, 'available'] = available
                     menu_df.to_csv("data/menu.csv", index=False)
                     st.success("Menu item updated!")
-                    st.rerun()
