06f8ffa5a1e2a953b9c20be99b45ea88a84dfcbd
AK_Maddy <99036358+abhileo00@users.noreply.github.com>
2025-07-20 07:21:27 +0530

    Update menu.py

--- pages/menu.py
+++ pages/menu.py
@@ -1,69 +1,54 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from utils.helpers import load_data, save_data
 
-def render():
-    st.title("Menu Management")
-    try:
-        menu_df = pd.read_csv("data/menu.csv")
-    except:
-        st.error("Failed to load menu data")
-        return
-    
-    tab1, tab2 = st.tabs(["View Menu", "Manage Items"])
-    
-    with tab1:
-        st.dataframe(menu_df)
-    
-    with tab2:
-        if st.session_state.user_role not in ["admin", "staff"]:
-            st.error("Staff access required")
-            return
+st.set_page_config(page_title="Menu Management", layout="wide")
+
+# Authentication check
+if 'user_role' not in st.session_state:
+    st.switch_page("app.py")
+
+menu = load_data('menu')
+
+st.title("Menu Management")
+
+if st.session_state.user_role == "admin":
+    # Admin can edit menu
+    with st.form("add_item_form"):
+        st.subheader("Add New Item")
+        item_name = st.text_input("Item Name")
+        description = st.text_area("Description")
+        price = st.number_input("Price", min_value=0.0, step=0.5)
+        category = st.text_input("Category")
+        available = st.checkbox("Available", value=True)
         
-        action = st.radio("Action", ["Add Item", "Edit Item"], horizontal=True)
-        
-        if action == "Add Item":
-            with st.form("add_item"):
-                name = st.text_input("Item Name")
-                description = st.text_area("Description")
-                category = st.text_input("Category")
-                price = st.number_input("Price", min_value=0.0, step=0.5, value=10.0)
-                prep_time = st.number_input("Prep Time (mins)", min_value=1, value=15)
-                available = st.checkbox("Available", True)
-                
-                if st.form_submit_button("Add Item"):
-                    new_item = {
-                        "item_id": f"ITEM_{datetime.now().strftime('%Y%m%d%H%M%S')}",
-                        "name": name,
-                        "description": description,
-                        "category": category,
-                        "price": price,
-                        "prep_time": prep_time,
-                        "available": available,
-                        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    }
-                    menu_df = pd.concat([menu_df, pd.DataFrame([new_item])], ignore_index=True)
-                    menu_df.to_csv("data/menu.csv", index=False)
-                    st.success("Item added to menu!")
-        
-        else:  # Edit Item
-            item_id = st.selectbox("Select Item", menu_df['item_id'])
-            item = menu_df[menu_df['item_id'] == item_id].iloc[0]
-            
-            with st.form("edit_item"):
-                name = st.text_input("Name", value=item['name'])
-                description = st.text_area("Description", value=item['description'])
-                category = st.text_input("Category", value=item['category'])
-                price = st.number_input("Price", value=float(item['price']))
-                prep_time = st.number_input("Prep Time", value=int(item['prep_time']))
-                available = st.checkbox("Available", value=bool(item['available']))
-                
-                if st.form_submit_button("Update Item"):
-                    menu_df.loc[menu_df['item_id'] == item_id, 'name'] = name
-                    menu_df.loc[menu_df['item_id'] == item_id, 'description'] = description
-                    menu_df.loc[menu_df['item_id'] == item_id, 'category'] = category
-                    menu_df.loc[menu_df['item_id'] == item_id, 'price'] = price
-                    menu_df.loc[menu_df['item_id'] == item_id, 'prep_time'] = prep_time
-                    menu_df.loc[menu_df['item_id'] == item_id, 'available'] = available
-                    menu_df.to_csv("data/menu.csv", index=False)
-                    st.success("Menu item updated!")
+        if st.form_submit_button("Add Item"):
+            new_item = {
+                'item_id': len(menu) + 1,
+                'item_name': item_name,
+                'description': description,
+                'price': price,
+                'category': category,
+                'available': available
+            }
+            menu = pd.concat([menu, pd.DataFrame([new_item])], ignore_index=True)
+            save_data(menu, 'menu')
+            st.success("Item added to menu")
+    
+    # Edit existing menu
+    st.subheader("Current Menu")
+    edited_menu = st.data_editor(
+        menu,
+        num_rows="dynamic",
+        hide_index=True,
+        use_container_width=True
+    )
+    
+    if st.button("Save Menu"):
+        save_data(edited_menu, 'menu')
+        st.success("Menu updated")
+
+else:
+    # Staff and customers can view
+    st.subheader("Our Menu")
+    st.dataframe(menu[menu['available']][['item_name', 'description', 'price']], hide_index=True)
